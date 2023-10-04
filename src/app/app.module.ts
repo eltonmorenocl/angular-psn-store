a19742b90b54268fb69f6bb63ff4a4f915e0a648
Elton <eltonmorenocl@gmail.com>
2023-10-04 16:32:24 -0300

    feat: cria component menu-bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { MenuBarComponent } from './src/app/components/menu-bar/menu-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    MenuBarComponent
   ],
   imports: [
     BrowserModule,
